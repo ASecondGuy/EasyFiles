db1b6df983ac847e3565810c1cf324901efb5f18
ASecondGuy <asecondguy@gmail.com>
2022-02-03 12:11:57 +0100

    Added Node version for easily seperating file listeners

--- addons/EasyFiles/plugin.gd
+++ addons/EasyFiles/plugin.gd
@@ -4,7 +4,10 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_autoload_singleton("EasyFiles", "res://addons/EasyFiles/EasyFiles.gd")
+	add_custom_type("EasyFilesNode", "Node", 
+	load("res://addons/EasyFiles/EasyFiles.gd"), load("res://addons/EasyFiles/icon.svg"))
 
 
 func _exit_tree():
 	remove_autoload_singleton("EasyFiles")
+	remove_custom_type("EasyFilesNode")
